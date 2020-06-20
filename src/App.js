e2c6d0367fef4428cd6adc31f089ad33de491166
bgsharath <shrathbg26@gmail.com>
2020-06-20 01:14:09 -0700

    state and props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     this is my new project
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meaningOfLife: 28
+    }   
+    this.props = props;
+  }
+
+  handleChange = () => {
+   this.setState((prevState, PrevProps)=>{
+     return {meaningOfLife: prevState.meaningOfLife + PrevProps.increment}
+     },
+     ()=>console.log(this.state.meaningOfLife)
+     
+   )
+  }
+
+   
+  
+
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.handleChange}>click</button>
+        {this.state.meaningOfLife}
+      </div>
+    );
+  }
 }
 
 export default App;
